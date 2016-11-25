314a85b796c632c6fadb0f1ed54278b6f9acae4e
Charmendar <wushiyuan90@gmail.com>
2016-11-24 21:03:24 -0500

    Updated Nov 24
    Added log function, changed video feed, added exception handling

--- Filter.cpp
+++ Filter.cpp
@@ -13,6 +13,9 @@
 //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //IN THE SOFTWARE.
 
+//Error code detail: -20 -> Log file not deleted
+//					 -10 -> Error open video
+//					 -5  -> Video play finished
 #include <sstream>
 #include <string>
 #include <stdio.h>
@@ -20,28 +23,31 @@
 #include <opencv\highgui.h>
 #include <opencv\cv.hpp>
 #include <opencv\cv.h>
+#include <fstream>
+#include <math.h>
+using namespace std;
 using std::string;
 using std::vector;
 
 using namespace cv;
 //initial min and max HSV filter values.
 //these will be changed using trackbars
-int H_MINCam1 = 0;
-int H_MAXCam1 = 256;
-int S_MINCam1 = 0;
-int S_MAXCam1 = 256;
-int V_MINCam1 = 0;
-int V_MAXCam1 = 256;
-
-int H_MINCam2 = 0;
-int H_MAXCam2 = 256;
-int S_MINCam2 = 0;
-int S_MAXCam2 = 256;
-int V_MINCam2 = 0;
-int V_MAXCam2 = 256;
+//int H_MINCam1 = 0;
+//int H_MAXCam1 = 256;
+//int S_MINCam1 = 0;
+//int S_MAXCam1 = 256;
+//int V_MINCam1 = 0;
+//int V_MAXCam1 = 256;
+//
+//int H_MINCam2 = 0;
+//int H_MAXCam2 = 256;
+//int S_MINCam2 = 0;
+//int S_MAXCam2 = 256;
+//int V_MINCam2 = 0;
+//int V_MAXCam2 = 256;
 //default capture width and height
-const int FRAME_WIDTH = 640;
-const int FRAME_HEIGHT = 480;
+const int FRAME_WIDTH = 320;
+const int FRAME_HEIGHT = 240;
 //max number of objects to be detected in frame
 const int MAX_NUM_OBJECTS=50;
 //minimum and maximum object area
@@ -57,6 +63,8 @@ const string windowName3Cam1 = "After Morphological Operations Camera1";
 const string windowName3Cam2 = "After Morphological Operations Camera2";
 const string trackbarWindowNameCam1 = "TrackbarsCam1";
 const string trackbarWindowNameCam2 = "TrackbarsCam2";
+const string cam1LogFile = "Cam1.log";
+const string cam2LogFile = "Cam2.log";
 
 //coordinate storage buffer
 double cam1_x[9999];
@@ -66,57 +74,62 @@ double cam2_y[9999];
 int delayBuf[9999];
 int capture_iteration = 0;
 
-void on_trackbar( int, void* )
-{//This function gets called whenever a
-	// trackbar position is changed
 
 
-
-
-
-}
+//void on_trackbar( int, void* )
+//{//This function gets called whenever a
+//	// trackbar position is changed
+//
+//}
 string intToString(int number){
-
-
 	std::stringstream ss;
 	ss << number;
 	return ss.str();
 }
-void createTrackbars(){
-	//create window for trackbars
 
-
-    namedWindow(trackbarWindowNameCam1, 0);
-	namedWindow(trackbarWindowNameCam2, 0);
-	//create memory to store trackbar name on window
-	char TrackbarName[50];
-	sprintf( TrackbarName, "H_MIN", H_MINCam1);
-	sprintf( TrackbarName, "H_MAX", H_MAXCam1);
-	sprintf( TrackbarName, "S_MIN", S_MINCam1);
-	sprintf( TrackbarName, "S_MAX", S_MAXCam1);
-	sprintf( TrackbarName, "V_MIN", V_MINCam1);
-	sprintf( TrackbarName, "V_MAX", V_MAXCam1);
-	//create trackbars and insert them into window
-	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
-	//the max value the trackbar can move (eg. H_HIGH), 
-	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
-	//                                  ---->    ---->     ---->      
-    createTrackbar( "H_MIN", trackbarWindowNameCam1, &H_MINCam1, H_MAXCam1, on_trackbar );
-    createTrackbar( "H_MAX", trackbarWindowNameCam1, &H_MAXCam1, H_MAXCam1, on_trackbar );
-    createTrackbar( "S_MIN", trackbarWindowNameCam1, &S_MINCam1, S_MAXCam1, on_trackbar );
-    createTrackbar( "S_MAX", trackbarWindowNameCam1, &S_MAXCam1, S_MAXCam1, on_trackbar );
-    createTrackbar( "V_MIN", trackbarWindowNameCam1, &V_MINCam1, V_MAXCam1, on_trackbar );
-    createTrackbar( "V_MAX", trackbarWindowNameCam1, &V_MAXCam1, V_MAXCam1, on_trackbar );
-
-	createTrackbar("H_MIN", trackbarWindowNameCam2, &H_MINCam2, H_MAXCam2, on_trackbar);
-	createTrackbar("H_MAX", trackbarWindowNameCam2, &H_MAXCam2, H_MAXCam2, on_trackbar);
-	createTrackbar("S_MIN", trackbarWindowNameCam2, &S_MINCam2, S_MAXCam2, on_trackbar);
-	createTrackbar("S_MAX", trackbarWindowNameCam2, &S_MAXCam2, S_MAXCam2, on_trackbar);
-	createTrackbar("V_MIN", trackbarWindowNameCam2, &V_MINCam2, V_MAXCam2, on_trackbar);
-	createTrackbar("V_MAX", trackbarWindowNameCam2, &V_MAXCam2, V_MAXCam2, on_trackbar);
-
-
+//For Data Analysis
+void writeLog(string path, string log) {
+	using namespace std;
+	ofstream myfile;
+	myfile.open(path, std::ios_base::app);
+	myfile << log << "\r\n";
+	myfile.close();
 }
+//void createTrackbars(){
+//	//create window for trackbars
+//
+//
+//    namedWindow(trackbarWindowNameCam1, 0);
+//	namedWindow(trackbarWindowNameCam2, 0);
+//	//create memory to store trackbar name on window
+//	char TrackbarName[50];
+//	sprintf( TrackbarName, "H_MIN", H_MINCam1);
+//	sprintf( TrackbarName, "H_MAX", H_MAXCam1);
+//	sprintf( TrackbarName, "S_MIN", S_MINCam1);
+//	sprintf( TrackbarName, "S_MAX", S_MAXCam1);
+//	sprintf( TrackbarName, "V_MIN", V_MINCam1);
+//	sprintf( TrackbarName, "V_MAX", V_MAXCam1);
+//	//create trackbars and insert them into window
+//	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
+//	//the max value the trackbar can move (eg. H_HIGH), 
+//	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
+//	//                                  ---->    ---->     ---->      
+//    createTrackbar( "H_MIN", trackbarWindowNameCam1, &H_MINCam1, H_MAXCam1, on_trackbar );
+//    createTrackbar( "H_MAX", trackbarWindowNameCam1, &H_MAXCam1, H_MAXCam1, on_trackbar );
+//    createTrackbar( "S_MIN", trackbarWindowNameCam1, &S_MINCam1, S_MAXCam1, on_trackbar );
+//    createTrackbar( "S_MAX", trackbarWindowNameCam1, &S_MAXCam1, S_MAXCam1, on_trackbar );
+//    createTrackbar( "V_MIN", trackbarWindowNameCam1, &V_MINCam1, V_MAXCam1, on_trackbar );
+//    createTrackbar( "V_MAX", trackbarWindowNameCam1, &V_MAXCam1, V_MAXCam1, on_trackbar );
+//
+//	createTrackbar("H_MIN", trackbarWindowNameCam2, &H_MINCam2, H_MAXCam2, on_trackbar);
+//	createTrackbar("H_MAX", trackbarWindowNameCam2, &H_MAXCam2, H_MAXCam2, on_trackbar);
+//	createTrackbar("S_MIN", trackbarWindowNameCam2, &S_MINCam2, S_MAXCam2, on_trackbar);
+//	createTrackbar("S_MAX", trackbarWindowNameCam2, &S_MAXCam2, S_MAXCam2, on_trackbar);
+//	createTrackbar("V_MIN", trackbarWindowNameCam2, &V_MINCam2, V_MAXCam2, on_trackbar);
+//	createTrackbar("V_MAX", trackbarWindowNameCam2, &V_MAXCam2, V_MAXCam2, on_trackbar);
+//
+//
+//}
 void drawObject(int x, int y,Mat &frame){
 
 	//use some of the openCV drawing functions to draw crosshairs
@@ -153,61 +166,60 @@ void morphOps(Mat &thresh){
 	Mat dilateElement = getStructuringElement( MORPH_RECT,Size(16,16));
 
 	erode(thresh,thresh,erodeElement);
-	erode(thresh,thresh,erodeElement);
-
-
 	dilate(thresh,thresh,dilateElement);
-	dilate(thresh,thresh,dilateElement);
+
 	
 
 
 }
 void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed, int delay, int cameraID) {
-
-	Mat temp;
-	threshold.copyTo(temp);
-	//these two vectors needed for output of findContours
-	vector< vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	//find contours of filtered image using openCV findContours function
-	findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
-	//use moments method to find our filtered object
-	double refArea = 0;
+	try
+	{
+		Mat temp;
+		threshold.copyTo(temp);
+		//these two vectors needed for output of findContours
+		vector< vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		//find contours of filtered image using openCV findContours function
+		findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
+		//use moments method to find our filtered object
+		double refArea = 0;
 	
-	//delay desplay variable
-	String delayText = "";
+		//delay desplay variable
+		String delayText = "";
+		string logpath;
 
-
-	bool objectFound = false;
-	if (hierarchy.size() > 0) {
-		int numObjects = hierarchy.size();
-        //if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
-        if(numObjects<MAX_NUM_OBJECTS){
-			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
-
-				Moments moment = moments((cv::Mat)contours[index]);
-				double area = moment.m00;
+		bool objectFound = false;
+		if (hierarchy.size() > 0) {
+			int numObjects = hierarchy.size();
+		 //if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+			if(numObjects<MAX_NUM_OBJECTS){
+				for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+					Moments moment = moments((cv::Mat)contours[index]);
+					double area = moment.m00;
 
 				//if the area is less than 20 px by 20px then it is probably just noise
 				//if the area is the same as the 3/2 of the image size, probably just a bad filter
 				//we only want the object with the largest area so we safe a reference area each
 				//iteration and compare it to the area in the next iteration.
-                if(area>MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea){
+				 if(area>MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea){
 					x = moment.m10/area;
 					y = moment.m01/area;
 					objectFound = true;
 					refArea = area;
-
+			
 					//store data into buffer
 					switch (cameraID)
 					{
 					case 1:
 						cam1_x[capture_iteration] = x;
 						cam1_y[capture_iteration] = y;
+						logpath = cam1LogFile;
 						break;
 					case 2:
 						cam2_x[capture_iteration] = x;
 						cam2_x[capture_iteration] = y;
+						logpath = cam2LogFile;
 						break;
 					default:
 						break;
@@ -220,24 +232,40 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed, int del
 			}
 			//let user know you found an object
 			if(objectFound ==true){
-				delayText = "delay =" + intToString(delay) + "ms";
+				delayText = "delay =" + intToString(delay) + "ms; Iteration = " + intToString(capture_iteration)+ " Camera: "+ intToString(cameraID);
+					//+ "; Coordinate: X= " + intToString(x) + ", Y= " + intToString(y);
+				string logstring = "Iteratioin: " + to_string(capture_iteration) + " Delay: " + to_string(delay);
 				putText(cameraFeed,delayText,Point(0,50),2,1,Scalar(0,255,0),2);
+				writeLog(logpath, logstring);
 				//draw object location on screen
 				drawObject(x,y,cameraFeed);}
 
 		}else putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",Point(0,50),1,2,Scalar(0,0,255),2);
 	}
+
+	}
+	catch (const std::exception&)
+	{
+		
+
+	}
+	
 }
 
-double * threeDCalculation(int cam1_x, int cam2_x, int cam1_y, int cam2_y, int d, int f) {
+double * threeDCalculation(int cam1_x, int cam2_x, int cam1_y, int cam2_y, double d, double f) {
 	//reconstruct 3-d position 
 	//fomular reference can be found here http://www.docin.com/p-381981146.html 
+	//Another one since previous one not seems to work proper http://www.dis.uniroma1.it/~iocchi/stereo/triang.html
+	//Z=f*Tx/d where f is focal, Tx is distance betwee cam and d is |x2-x1|
 	//need (x1,y1) (x2,y2) difference between 2 cams d and Focal of cam f
 	double coord[3];
-	if (cam2_x - cam1_x != 0) {
-		coord[0] = d*cam1_x / (cam2_x - cam1_x);
-		coord[1] = d*cam1_y / (cam2_x - cam1_x);
-		coord[2] = d*f / (cam2_x - cam1_x);
+	int delta_x = abs(cam2_x - cam1_x);
+	if (delta_x != 0) {
+		coord[0] = (cam2_x + cam1_x) / 2;
+		coord[1] = (cam2_y + cam1_y) / 2;
+		coord[2] = d*f / delta_x;
+		//coord[0] = cam1_x*coord[2] / f;
+		//coord[1] = cam1_y*coord[2] / f;
 	}
 	else {
 		coord[0] = 999;
@@ -247,8 +275,18 @@ double * threeDCalculation(int cam1_x, int cam2_x, int cam1_y, int cam2_y, int d
 	return coord;
 }
 
+bool fexists(const std::string& filename) {
+	std::ifstream ifile(filename.c_str());
+	return (bool)ifile;
+}
+
 int main(int argc, char* argv[])
 {
+	//Initualize, if forgot to delete file return
+	if(fexists(cam1LogFile)||fexists(cam2LogFile))
+	{
+		return -20;
+	}
 	//some boolean variables for different functionality within this
 	//program
     bool trackObjects = true;
@@ -269,19 +307,17 @@ int main(int argc, char* argv[])
 	//timestampe record
 	int t1, t2, tdiff = 999;
 	
-	//create slider bars for HSV filtering
-	createTrackbars();
 	//video capture object to acquire webcam feed
 	VideoCapture captureCam1;
 	VideoCapture captureCam2;
 	//open capture object at location zero (default location for webcam)
-	captureCam1.open("Green.mp4");
-	captureCam2.open("Green.mp4");
+	captureCam1.open("BB1_L.mp4");
+	captureCam2.open("BB1_R.mp4");
 
 	if (!captureCam1.isOpened()) {
 		printf("ERROR ACQUIRING VIDEO FEED\n");
 		getchar();
-		return -1;
+		return -10;
 	}
 
 	//add while loop to detect last video feed frame
@@ -304,7 +340,7 @@ int main(int argc, char* argv[])
 			//threshold matrix
 			//inRange(HSVCam1, Scalar(H_MINCam1, S_MINCam1, V_MINCam1), Scalar(H_MAXCam1, S_MAXCam1, V_MAXCam1), threshold1);
 			//inRange(HSVCam2, Scalar(H_MINCam2, S_MINCam2, V_MINCam2), Scalar(H_MAXCam2, S_MAXCam2, V_MAXCam2), threshold2);
-			//hardcode HSV for puregreen comment out for different color
+			//hardcode HSV for puregreen comment out for different color, value obtained from backbone
 			inRange(HSVCam1, Scalar(67, 0, 86), Scalar(95, 256, 256), threshold1);
 			inRange(HSVCam2, Scalar(67, 0, 86), Scalar(95, 256, 256), threshold2);
 
@@ -317,34 +353,43 @@ int main(int argc, char* argv[])
 			t1 = getTickCount();
 			if (useMorphOps)
 				morphOps(threshold1);
-			morphOps(threshold2);
+				morphOps(threshold2);
 			//pass in thresholded frame to our object tracking function
 			//this function will return the x and y coordinates of the
 			//filtered object
-			if (trackObjects)
-				trackFilteredObject(x, y, threshold1, cameraFeedCam1, tdiff, 1);
+			//if (trackObjects)
+			trackFilteredObject(x, y, threshold1, cameraFeedCam1, tdiff, 1);
 			trackFilteredObject(x, y, threshold2, cameraFeedCam2, tdiff, 2);
 			//calculate 3d coordinate, might want a separate thread so thatcapture would not be stopped
 			//for now put distance between camera to be 10 and focal to be 10 since
-			double * threeD = threeDCalculation(cam1_x[capture_iteration], cam2_x[capture_iteration], cam1_y[capture_iteration], cam2_y[capture_iteration], 10, 10);
+			double * threeD = threeDCalculation(cam1_x[capture_iteration], cam2_x[capture_iteration], cam1_y[capture_iteration], cam2_y[capture_iteration], 140, 2.31);
 			t2 = getTickCount();
 			tdiff = t2 - t1;
 			tdiff = tdiff / (getTickFrequency() / 1000);
 			delayBuf[capture_iteration] = tdiff;
-			printf("3D coordinate x: %f y: %f z: %f delay: %dms\n", threeD[0], threeD[1], threeD[2], tdiff);
+			printf("Iteration: %d 3D coordinate x: %f y: %f z: %f delay: %dms\n", capture_iteration, threeD[0], threeD[1], threeD[2], tdiff);
 			//show frames 
-			imshow(windowName2Cam1, threshold1);
-			imshow(windowName2Cam2, threshold2);
-			imshow(windowNameCam1, cameraFeedCam1);
-			imshow(windowNameCam2, cameraFeedCam2);
-			//imshow(windowName1,HSV);
+			try
+			{
+				imshow(windowName2Cam1, threshold1);
+							imshow(windowName2Cam2, threshold2);
+							imshow(windowNameCam1, cameraFeedCam1);
+							imshow(windowNameCam2, cameraFeedCam2);
+							//imshow(windowName1,HSV);
 
-			//iteration index
-			capture_iteration++;
+							//iteration index
+							capture_iteration++;
 
-			//delay 30ms so that screen can refresh.
-			//image will not appear without this waitKey() command
-			waitKey(30);
+							//delay 30ms so that screen can refresh.
+							//image will not appear without this waitKey() command
+							waitKey(1);
+			}
+			catch (const std::exception&)
+			{
+				//exit once video play if finished
+				return -5;
+			}
+			
 		}
 
 	}
